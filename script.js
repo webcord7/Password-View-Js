4a13a25b6d4a8ee6eeeabeb51dcbf250947d67ec
Abdulrehman <webcord7@gmail.com>
2023-12-04 21:42:01 +0500

    Complete Project

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-dfd
+let password = document.getElementById("password");
+let eyeicon = document.getElementById("eyeicon");
+function passwordShow(){
+  if(password.type == "password"){
+    password.type = "text";
+    eyeicon.src = "images/eye-open.png"
+}else{
+    password.type = "password" ;
+    eyeicon.src = "images/eye-close.png"
+ }
+}
